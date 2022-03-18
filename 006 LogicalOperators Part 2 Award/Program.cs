3e5f04dd7f484dea76ba462329164f559b5ff7f7
ChillingBore563 <teddybeargh@gmail.com>
2022-03-18 22:43:16 +0300

    Added homework for lesson 17

---
+++ 006 LogicalOperators Part 2 Award/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _006_LogicalOperators_Part_2_Award
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выслугу лет:");
+
+            string stringWorkTime = Console.ReadLine();
+
+            int workTime;
+
+            try
+            {
+                workTime = Convert.ToInt32(stringWorkTime);
+
+                if (workTime < 5)
+                {
+                    Console.WriteLine("Премия 10%");
+                }
+                else if (workTime >= 5 && workTime < 10)
+                {
+                    Console.WriteLine("Премия 15%");
+                }
+                else if (workTime >= 10 && workTime < 15)
+                {
+                    Console.WriteLine("Премия 25%");
+                }
+                else if (workTime >= 15 && workTime < 20)
+                {
+                    Console.WriteLine("Премия 35%");
+                }
+                else if (workTime >= 20 && workTime < 25)
+                {
+                    Console.WriteLine("Премия 45%");
+                }
+                else if (workTime >= 25)
+                {
+                    Console.WriteLine("Премия 50%");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("SAM TI HUY");
+            }
+        }
+    }
+}
