cd06d23df8174a222118ec8b0a0b6776783afc59
ChillingBore563 <teddybeargh@gmail.com>
2022-03-22 23:07:34 +0300

    Added first half of Lesson 18 homework

---
+++ 007BitOperations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _007BitOperations
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 5, y = 10, z = 15;
+
+            Console.WriteLine("Дано: x = 5, y = 10, z = 15");
+            Console.WriteLine("________________________________");
+            Console.WriteLine("Вычислите:");
+            Console.WriteLine("x += y >> x++ * z;");
+            Console.WriteLine("z = ++x & y * 5;");
+            Console.WriteLine("y /= x + 5 | z;");
+            Console.WriteLine("z = x++ & y * 5");
+            Console.WriteLine("x = y << x++ ^ z;");
+            Console.WriteLine("________________________________");
+
+            x += y >> x++ * z;
+            z = ++x & y * 5;
+            y /= x + 5 | z;
+            z = x++ & y * 5;
+            x = y << x++ ^ z;
+
+            Console.WriteLine($"Результат: x = {x}, y = {y}, z = {z} ");
+
+            Console.ReadKey();
+        }
+    }
+}
