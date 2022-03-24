53aed6404b28684eb16324fc34148b5c13c9b0b0
ChillingBore563 <teddybeargh@gmail.com>
2022-03-24 22:23:08 +0300

    Added homework for lesson 19

---
+++ Variable Creation Rules/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Variable_Creation_Rules
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double flatLivingSquare = 1.1; // миллиметры
+
+            double flat250UsedElectricity = 91.03; //нановаты
+        }
+    }
+}
