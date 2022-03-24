d314524e5360a3ce978c5e062c635f9c5d02836b
ChillingBore563 <teddybeargh@gmail.com>
2022-03-24 21:13:42 +0300

    Added homework for lesson 18 part 2

---
+++ 007 BitOperations Part2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _007_BitOperations_Part2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ:");
+            string inputString = Console.ReadLine();
+            char charFromString = Convert.ToChar(inputString);
+
+            int key = 216; // Ключ шифрования
+
+            char encryptedChar = (char)(charFromString ^ key); // Процесс шифрования
+            Console.WriteLine($"Результат кодирования: {encryptedChar}");
+
+            char decryptedChar = (char)(encryptedChar ^ key);
+            Console.WriteLine($"Дешифровка символа: {decryptedChar}"); // Процесс дешифровки
+
+            Console.ReadKey();
+        }
+    }
+}
