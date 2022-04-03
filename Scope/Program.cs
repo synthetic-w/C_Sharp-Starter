bcdb330c96d8785a5a71447226743f4af5688c0d
ChillingBore563 <teddybeargh@gmail.com>
2022-04-03 15:04:39 +0300

    Added homework for lesson 19 (Scope) and some minor changes with a several projects names

---
+++ Scope/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Scope
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение переменной а:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            {
+                a = a++ * 2 + 1488 / 228; // Локальная область 1 с преобразованием
+            }
+
+            {
+                Console.WriteLine($"Значение переменной а после преобразования: {a}"); // Локальная область 2 с выводом
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
