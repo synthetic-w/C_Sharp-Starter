48864c302987b6f65fd8f173b08badc99797a6e0
ChillingBore563 <teddybeargh@gmail.com>
2022-04-06 22:29:31 +0300

    Renamed project files 018 - 020, added homework for lesson 021 (Constant)

---
+++ 021 Constant/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _021_Constant
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение числа Пи: ");
+
+            // 1. Сравнение значения константы PI с введённым значением
+            const double PI = 3.141593;
+            bool isConstBigger;
+            {
+                double inputValue = Convert.ToDouble(Console.ReadLine());
+                isConstBigger = PI == inputValue;
+            }
+
+            // 2. Сравнение значения константы MY_CONST с результирующим значением блока 1
+            const bool MY_CONST = true;
+            bool isBoolTrue;
+            {
+                isBoolTrue = MY_CONST == isConstBigger;
+                Console.WriteLine($"Результат финального сравнения {isBoolTrue}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
