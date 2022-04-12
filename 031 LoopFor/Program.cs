d5a131afa2cd4c1d64f02666a4f803518f5fbeb1
ChillingBore563 <teddybeargh@gmail.com>
2022-04-12 23:39:58 +0300

    Minor syntax fixes

--- 031 LoopFor/Program.cs
+++ 031 LoopFor/Program.cs
@@ -10,7 +10,7 @@
 Console.WriteLine("1. Нажмите любую клавишу для вывода всех целых чисел от 0 до 100");
 Console.ReadKey();
 
-for (int index = 0; index < 101; index++) {
+for (int index = 0; index <= 100; index++) {
     Console.WriteLine(index);
 }
 Console.ReadKey();
@@ -19,31 +19,28 @@ Console.ReadKey();
 Console.WriteLine("2. Нажмите любую клавишу для вывода всех целых чисел от 1001 до 2500 с шагом 3");
 Console.ReadKey();
 
-{
-    for (int index = 0; index < 2501; index += 3) {
-        Console.WriteLine(index);
-    }
+for (int index = 1001; index <= 2500; index += 3) {
+    Console.WriteLine(index);
 }
+
 Console.ReadKey();
 
 // 3. Вывод на экран чисел от 100 до 0 с шагом 4
 Console.WriteLine("3. Нажмите любую клавишу для вывода всех целых чисел от 100 до 0 с шагом 4");
 Console.ReadKey();
 
-{
-    for (int index = 100; index > -1; index -= 4) {
-        Console.WriteLine(index);
-    }
+for (int index = 100; index >= 0; index -= 4) {
+    Console.WriteLine(index);
 }
+
 Console.ReadKey();
 
 // 4. Вывод на экран чисел от 1 до 5 с шагом 0,2
 Console.WriteLine("3. Нажмите любую клавишу для вывода всех чисел от 1 до 5 с шагом 0,2");
 Console.ReadKey();
 
-{
-    for (double index = 1; index < 5; index += 0.2) {
-        Console.WriteLine(Math.Round(index, 1));
-    }
+for (double index = 1; index <= 5; index += 0.2) {
+    Console.WriteLine(Math.Round(index, 1));
 }
+
 Console.ReadKey();
