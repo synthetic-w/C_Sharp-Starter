9dfa2030e1d036f29d4c7907ebb95f4004999672
ChillingBore563 <teddybeargh@gmail.com>
2022-05-02 21:44:35 +0300

    Added homework for lessons 033 (ThreeDimensionalArray), 034 (FourDimensionalArray), 036 (Methods)

---
+++ 034 FourDimensionalArray/034 FourDimensionalArray/Program.cs
@@ -0,0 +1,143 @@
+﻿/*
+Имеется библиотека, в которой находится 5 стеллажей с книгами. Каждый стеллаж
+является типовым – в нем имеется 10 полок и на каждой полке помещается по 10 книг.
+Для хранения информации о том, на каком стеллаже, на какой полке и в каком месте
+этой полки хранится книга – в библиотечной базе данных используется массив строк с
+названиями книг.
+Реализуйте такой способ хранения информации о книгах в вашей программе. В вашей
+библиотеке на разных стеллажах, разных полках и разных местах на полках поместили 3
+книги: «О дивный новый мир», «Приключения Тома Сойера» и «CLR via C#».
+
+Напишите программу, в которой пользователь, введя название книги, может получить
+информацию о ее расположении – номер стеллажа, номер полки и номер места на полке.
+
+Теперь, дополнительно к предыдущему условию, библиотека состоит еще и из 3х одинаковых
+комнат со стеллажами. В этой увеличенной библиотеке разместите три книги.
+Напишите программу, в которой пользователь, введя название книги, может получить
+информацию о ее расположении – номер комнаты, номер стеллажа, номер полки и
+номер места на полке.
+Дополнительно выведите на экран все имеющиеся в библиотеке книги.
+ */
+
+string[,,,] library = new string[3, 5, 10, 10];
+
+Console.WriteLine("Введите количество книг:");
+int bookCount = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < bookCount; i++)
+{
+    BookRandomizer(library);
+}
+
+Console.WriteLine(new String('=', 15));
+Console.WriteLine("Введите название искомой книги:");
+
+string bookName = Console.ReadLine();
+
+BookPositionFinder(bookName, library);
+
+Console.WriteLine(new String('=', 15));
+Console.WriteLine("Книги, находящиеся в библиотеке:");
+
+ShowLibraryContent(library);
+
+// Поиск расположения книги по названию
+static void BookPositionFinder(string bookName, string[,,,] library)
+{
+    for (int i = 0; i < library.GetLength(0); i++)
+    {
+        for (int j = 0; j < library.GetLength(1); j++)
+        {
+            for (int k = 0; k < library.GetLength(2); k++)
+            {
+                for (int l = 0; l < library.GetLength(3); l++)
+                {
+                    if (library[i, j, k, l] != null && library[i, j, k, l].ToLower() == bookName.ToLower())
+                    {
+                        Console.WriteLine($"Книга с названием {bookName} находится в {i + 1} комнате, в {j + 1} стеллаже, на {k + 1} полке и стоит {l + 1} номером");
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Расставление книг в рандомном порядке
+static void BookRandomizer(string[,,,] library)
+{
+    Random random = new Random();
+
+    int bookRoom = random.Next(library.GetLength(0) - 1);
+
+    int bookStillage = random.Next(library.GetLength(1) - 1);
+
+    int bookShelf = random.Next(library.GetLength(2) - 1);
+
+    int bookPosition = random.Next(library.GetLength(3) - 1);
+
+    Console.WriteLine("Введите название книги:");
+    string bookName = Console.ReadLine();
+
+    if (library[bookRoom, bookStillage, bookShelf, bookPosition] == null)
+    {
+        library[bookRoom, bookStillage, bookShelf, bookPosition] = bookName;
+    }
+    else
+    {
+        if (EmptyCellsPresence(library))
+        {
+            BookRandomizer(library);
+        }
+        else
+        {
+            Console.WriteLine("Библиотека заполнена");
+            return;
+        }
+    }
+}
+
+// Проверка отсутствия хотя бы одного свободного места в массиве (библиотеке)
+static bool EmptyCellsPresence(string[,,,] library)
+{
+    for (int i = 0; i < library.GetLength(0); i++)
+    {
+        for (int j = 0; j < library.GetLength(1); j++)
+        {
+            for (int k = 0; k < library.GetLength(2); k++)
+            {
+                for (int l = 0; l < library.GetLength(3); l++)
+                {
+                    if (library[i, j, k, l] != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+// Перечисление книг, находящихся в библиотеке
+static void ShowLibraryContent(string[,,,] library)
+{
+    for (int i = 0; i < library.GetLength(0); i++)
+    {
+        for (int j = 0; j < library.GetLength(1); j++)
+        {
+            for (int k = 0; k < library.GetLength(2); k++)
+            {
+                for (int l = 0; l < library.GetLength(3); l++)
+                {
+                    if (library[i, j, k, l] != null)
+                    {
+                        Console.WriteLine(library[i, j, k, l]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+Console.ReadKey();
---
+++ 036 Methods/036 Methods/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Пользователь вводит количество сотрудников предприятия, стоимость 1 трудодня
+сотрудника и количество дней, за которые производится оплата.
+
+Создайте метод, который считает зарплату рабочему, исходя из количества дней и
+подневной оплаты труда. Создайте метод, который принимает в себя количество
+сотрудников, количество рабочих дней и цену трудодня, и вычисляет общий объем
+денег, необходимый для оплаты полученному количеству сотрудников, используя
+внутри себя первый метод.
+
+Посчитайте и выведите на экран общий объем денег, необходимый для оплаты
+зарплаты введенному пользователем количеству сотрудников.
+ */
+
+Console.WriteLine("Введите количество сотрудников");
+int employees = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите стоимость трудодня для одного сотрудника");
+int dailySalary = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество дней, за которые производится оплата");
+int workDays = Convert.ToInt32(Console.ReadLine());
+
+int totalSalary = SalariesSummary(workDays, dailySalary, employees);
+Console.WriteLine($"Общая зарплата работников за {workDays} дня/дней составит {totalSalary}");
+
+Console.ReadKey();
+
+// Зарплата одного сотрудника
+int EmployerSalary(int workDays, int dailySalary)
+{
+    return workDays * dailySalary;
+}
+
+// Рассчёт общего объема зарплаты сотрудников
+int SalariesSummary(int workDays, int dailySalary, int employees)
+{
+    int employerSalary = EmployerSalary(workDays, dailySalary);
+    return employerSalary * employees;
+}
